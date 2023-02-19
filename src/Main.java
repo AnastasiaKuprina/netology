e45f7de04302c981abd44e47bb1713468526b3ac
Anastasia Kuprina <ApplicationMath@gmail.com>
2023-02-19 18:20:03 +0500

    Longer commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] s = {100, 200, 300};
+        long [] s = {100, 200, 300};
 
         SalesManager sm = new SalesManager(s);
         System.out.println("Максимальная продажа " + sm.max());
